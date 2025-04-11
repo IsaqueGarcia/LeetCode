628d97909e699f904dc2b4bdf754c3f3c4bbc8f0
IsaqueGarcia <isaqueemprego1@gmail.com>
2025-04-11 18:16:19 -0300

    solved symmetrics integers

---
+++ src/CountSymmetricsIntegers.java
@@ -0,0 +1,41 @@
+public class CountSymmetricsIntegers {
+
+    public static void main(String[] args) {
+
+        System.out.println("Symmetics Numbers: " + countSymmetricIntegers(1, 100));
+
+    }
+
+    public static int countSymmetricIntegers(int low, int high) {
+
+        int symmetrics = 0;
+
+        for (int i = low; i <= high; i++) {
+
+            String stringNumber = Integer.toString(i);
+            int len = stringNumber.length();
+
+            if (len % 2 != 0) continue;
+
+            int firstSum = 0;
+            int secondSum = 0;
+            int half = len / 2;
+
+            for (int j = 0; j < half; j++) {
+                firstSum += stringNumber.charAt(j);
+            }
+
+            for (int j = half; j < len; j++) {
+                secondSum += stringNumber.charAt(j);
+            }
+
+            if (firstSum == secondSum) {
+                symmetrics++;
+            }
+
+        }
+
+        return symmetrics;
+    }
+
+}
